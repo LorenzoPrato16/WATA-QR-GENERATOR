2cb46e35bd5132a57ed2a647edf182c4d3ea7e2c
Lorenzo <lorenzo.prato@epfl.ch>
2026-03-09 11:59:34 +0100

    Logo

--- Apptest.py
+++ Apptest.py
@@ -3,7 +3,7 @@ import qrcode
 from urllib.parse import quote
 from io import BytesIO
 
-st.set_page_config(page_title="Générateur de QR code", page_icon="📱")
+st.set_page_config(page_title="Générateur de QR code", page_icon="WATA-logo-400x400px.ico", layout="centered")
 st.image("WATA_logo_150px.png", width=200)
 st.title("Générateur de QR code - SAV WATALUX")
 st.text("Générez le QR code à afficher sur le dispositif en sélectionnant le modèle et le numéro de série.")
